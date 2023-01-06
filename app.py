43c32b3a5f75a268b08612cd3d41da6fd1334204
svkote <kotekofly@gmail.com>
2023-01-06 13:20:35 +0300

    route orders

--- app.py
+++ app.py
@@ -6,8 +6,41 @@ from models import User, Offer, Order
 @app.route('/users/', method=['GET'])
 def get_users():
     """Возвращает весь список пользователей"""
-    users = db.session.query(User).all()
-    return jsonify([user.serialize() for user in users])
+    try:
+        users = db.session.query(User).all()
+        return jsonify([user.serialize() for user in users])
+    except Exception as e:
+        return f'{e}'
+
+
+@app.route('/users/<int:user_id>', method=['GET'])
+def get_users_by_id(user_id):
+    """Возвращает весь список пользователей"""
+    try:
+        users = db.session.query(User).filter(User.id == user_id).first()
+        return jsonify([user.serialize() for user in users])
+    except Exception as e:
+        return f'{e}'
+
+
+@app.route('/orders/', method=['GET'])
+def get_orders():
+    """Возвращает весь список пользователей"""
+    try:
+        orders = db.session.query(Order).all()
+        return jsonify([order.get_order() for order in orders])
+    except Exception as e:
+        return f'{e}'
+
+
+@app.route('/orders/<int:order_id>', method=['GET'])
+def get_orders_by_id(order_id):
+    """Возвращает весь список пользователей"""
+    try:
+        orders = db.session.query(Order).filter(Order.id == order_id).first()
+        return jsonify([order.get_order() for order in orders])
+    except Exception as e:
+        return f'{e}'
 
 
 @app.errorhandler(404)
