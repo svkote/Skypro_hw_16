bcb4c25d89e460cd790847884183701341f0ad9f
svkote <kotekofly@gmail.com>
2023-01-06 16:41:05 +0300

    Я ее доделала Осподи Исуси

--- app.py
+++ app.py
@@ -23,17 +23,16 @@ def get_users():
 @app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
 def get_users_by_id(user_id):
     """Возвращает весь список пользователей"""
+    data = request.json
+    user = db.session.query(User).filter(User.id == user_id).first()
+
     if request.method == 'GET':
         try:
-            user = db.session.query(User).filter(User.id == user_id).first()
-            print(user.serialize())
             return jsonify(user.serialize())
         except Exception as e:
             return f'{e}'
-    elif request.method == 'PUT':
-        data = request.json
-        user = db.session.query(User).filter(User.id == user_id).first()
 
+    elif request.method == 'PUT':
         if user is None:
             with db.session.begin():
                 db.session.add(User(**data))
@@ -42,55 +41,109 @@ def get_users_by_id(user_id):
             with db.session.begin():
                 db.session.query(User).filter(User.id == user_id).update(request.json)
             return 'Успешно обновлен'
-    elif request.method == 'DELETE':
-        user = db.session.query(User).filter(User.id == user_id).first()
 
+    elif request.method == 'DELETE':
         if user is None:
             return 'Не найден'
         else:
             with db.session.begin():
                 db.session.query(User).filter(User.id == user_id).delete()
-            return 'Успешно обновлен'
+            return 'Успешно Удален'
 
 
-@app.route('/orders/', methods=['GET'])
+@app.route('/orders/', methods=['GET', 'POST'])
 def get_orders():
     """Возвращает весь список пользователей"""
-    try:
-        orders = db.session.query(Order).all()
-        return jsonify([order.get_order() for order in orders])
-    except Exception as e:
-        return f'{e}'
+    if request.method == 'GET':
+        try:
+            orders = db.session.query(Order).all()
+            return jsonify([order.get_order() for order in orders])
+        except Exception as e:
+            return f'{e}'
+    elif request.method == 'POST':
+        data = request.json
+        with db.session.begin():
+            db.session.add(Order(**data))
+        return 'Успешно добавлен'
 
 
 @app.route('/orders/<int:order_id>', methods=['GET'])
 def get_orders_by_id(order_id):
     """Возвращает весь список пользователей"""
-    try:
-        order = db.session.query(Order).filter(Order.id == order_id).first()
-        return jsonify(order.get_order())
-    except Exception as e:
-        return f'{e}'
+    order = db.session.query(Order).filter(Order.id == order_id).first()
+    data = request.json
+
+    if request.method == 'GET':
+        try:
+            return jsonify(order.get_order())
+        except Exception as e:
+            return f'{e}'
+    elif request.method == 'PUT':
+
+        if order is None:
+            with db.session.begin():
+                db.session.add(Order(**data))
+            return 'Успешно добавлен'
+        else:
+            with db.session.begin():
+                db.session.query(Order).filter(Order.id == order_id).update(request.json)
+            return 'Успешно обновлен'
+    elif request.method == 'DELETE':
+
+        if order is None:
+            return 'Не найден'
+        else:
+            with db.session.begin():
+                db.session.query(Order).filter(Order.id == order_id).delete()
+            return 'Успешно Удален'
 
 
-@app.route('/offers/', methods=['GET'])
+@app.route('/offers/', methods=['GET', 'POST'])
 def get_offers():
     """Возвращает весь список пользователей"""
-    try:
-        offers = db.session.query(Offer).all()
-        return jsonify([offer.get_offer() for offer in offers])
-    except Exception as e:
-        return f'{e}'
+    if request.method == 'GET':
+        try:
+            offers = db.session.query(Offer).all()
+            return jsonify([offer.get_offer() for offer in offers])
+        except Exception as e:
+            return f'{e}'
+    elif request.method == 'POST':
+        data = request.json
+        with db.session.begin():
+            db.session.add(Offer(**data))
+        return 'Успешно добавлен'
 
 
 @app.route('/offers/<int:offer_id>', methods=['GET'])
 def get_offers_by_id(offer_id):
     """Возвращает весь список пользователей"""
-    try:
-        offer = db.session.query(Offer).filter(Offer.id == offer_id).first()
-        return jsonify(offer.get_offer())
-    except Exception as e:
-        return f'{e}'
+    data = request.json
+    offer = db.session.query(Offer).filter(Offer.id == offer_id).first()
+
+    if request.method == 'GET':
+        try:
+            return jsonify(offer.get_offer())
+        except Exception as e:
+            return f'{e}'
+
+    elif request.method == 'PUT':
+        if offer is None:
+            with db.session.begin():
+                db.session.add(Offer(**data))
+            return 'Успешно добавлен'
+        else:
+            with db.session.begin():
+                db.session.query(Offer).filter(Offer.id == offer_id).update(request.json)
+            return 'Успешно обновлен'
+
+    elif request.method == 'DELETE':
+
+        if offer is None:
+            return 'Не найден'
+        else:
+            with db.session.begin():
+                db.session.query(Offer).filter(Offer.id == offer_id).delete()
+            return 'Успешно Удален'
 
 
 @app.errorhandler(404)
