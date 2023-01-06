7740a5e0ecd8321b49c82c0777d9fff0b0a7afd3
svkote <kotekofly@gmail.com>
2023-01-06 15:36:07 +0300

    migrate.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import render_template, jsonify
 from config import app, db
+from migrate import init_database
 from models import User, Offer, Order
 
 
-@app.route('/users/', method=['GET'])
+@app.route('/users/', methods=['GET'])
 def get_users():
     """Возвращает весь список пользователей"""
     try:
@@ -13,17 +14,18 @@ def get_users():
         return f'{e}'
 
 
-@app.route('/users/<int:user_id>', method=['GET'])
+@app.route('/users/<int:user_id>', methods=['GET'])
 def get_users_by_id(user_id):
     """Возвращает весь список пользователей"""
     try:
-        users = db.session.query(User).filter(User.id == user_id).first()
-        return jsonify([user.serialize() for user in users])
+        user = db.session.query(User).filter(User.id == user_id).first()
+        print(user.serialize())
+        return jsonify(user.serialize())
     except Exception as e:
         return f'{e}'
 
 
-@app.route('/orders/', method=['GET'])
+@app.route('/orders/', methods=['GET'])
 def get_orders():
     """Возвращает весь список пользователей"""
     try:
@@ -33,17 +35,17 @@ def get_orders():
         return f'{e}'
 
 
-@app.route('/orders/<int:order_id>', method=['GET'])
+@app.route('/orders/<int:order_id>', methods=['GET'])
 def get_orders_by_id(order_id):
     """Возвращает весь список пользователей"""
     try:
-        orders = db.session.query(Order).filter(Order.id == order_id).first()
-        return jsonify([order.get_order() for order in orders])
+        order = db.session.query(Order).filter(Order.id == order_id).first()
+        return jsonify(order.get_order())
     except Exception as e:
         return f'{e}'
 
 
-@app.route('/offers/', method=['GET'])
+@app.route('/offers/', methods=['GET'])
 def get_offers():
     """Возвращает весь список пользователей"""
     try:
@@ -53,12 +55,12 @@ def get_offers():
         return f'{e}'
 
 
-@app.route('/offers/<int:order_id>', method=['GET'])
+@app.route('/offers/<int:offer_id>', methods=['GET'])
 def get_offers_by_id(offer_id):
     """Возвращает весь список пользователей"""
     try:
-        offers = db.session.query(Offer).filter(Offer.id == offer_id).first()
-        return jsonify([offer.get_offer() for offer in offers])
+        offer = db.session.query(Offer).filter(Offer.id == offer_id).first()
+        return jsonify(offer.get_offer())
     except Exception as e:
         return f'{e}'
 
@@ -74,4 +76,6 @@ def internal_server_error(error):
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        init_database()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -31,9 +31,9 @@ class Order(db.Model):
     __tablename__ = 'order'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    description = db.Column(db.String)
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
+    description = db.Column(db.Text)
+    start_date = db.Column(db.Integer)
+    end_date = db.Column(db.Integer)
     address = db.Column(db.String)
     price = db.Column(db.Integer)
     customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
